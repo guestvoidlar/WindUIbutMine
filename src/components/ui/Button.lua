4aa4899b6fe756ea92113b9d0062fa0a57c29b57
undefetaed <undefetaed@gmail.com>
2026-06-06 10:02:43 +0400

    Update Button.lua

--- src/components/ui/Button.lua
+++ src/components/ui/Button.lua
@@ -4,8 +4,7 @@ local Creator = require("../../modules/Creator")
 local New = Creator.New
 local Tween = Creator.Tween
 
-
-function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded, Radius)
+function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded, Radius, IconColor)
     Variant = Variant or "Primary"
     local Radius = Radius or (not FullRounded and 10 or 99)
     local IconButtonFrame
@@ -14,16 +13,16 @@ function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded,
             Image = Creator.Icon(Icon)[1],
             ImageRectSize = Creator.Icon(Icon)[2].ImageRectSize,
             ImageRectOffset = Creator.Icon(Icon)[2].ImageRectPosition,
-            Size = UDim2.new(0,24-3,0,24-3),
+            Size = UDim2.new(0,21,0,21),
             BackgroundTransparency = 1,
-            ImageColor3 = Variant == "White" and Color3.new(0,0,0) or nil,
+            ImageColor3 = IconColor or (Variant == "White" and Color3.new(0,0,0) or nil),
             ImageTransparency = Variant == "White" and .4 or 0,
-            ThemeTag = {
+            ThemeTag = (not IconColor) and {
                 ImageColor3 = Variant ~= "White" and "Icon" or nil,
-            }
+            } or nil
         })
     end
-    
+
     local ButtonFrame = New("TextButton", {
         Size = UDim2.new(0,0,1,0),
         AutomaticSize = "X",
@@ -39,31 +38,24 @@ function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded,
             Name = "Squircle",
             ImageTransparency = Variant == "Primary" and 0 or Variant == "White" and 0 or 1
         }),
-    
+
         Creator.NewRoundFrame(Radius, "Squircle", {
-            -- ThemeTag = {
-            --     ImageColor3 = "Layer",
-            -- },
             ImageColor3 = Color3.new(1,1,1),
             Size = UDim2.new(1,0,1,0),
             Name = "Special",
             ImageTransparency = Variant == "Secondary" and 0.95 or 1
         }),
-    
+
         Creator.NewRoundFrame(Radius, "Shadow-sm", {
-            -- ThemeTag = {
-            --     ImageColor3 = "Layer",
-            -- },
             ImageColor3 = Color3.new(0,0,0),
             Size = UDim2.new(1,3,1,3),
             AnchorPoint = Vector2.new(0.5,0.5),
             Position = UDim2.new(0.5,0,0.5,0),
             Name = "Shadow",
-            --ImageTransparency = Variant == "Secondary" and 0 or 1,
             ImageTransparency = 1,
             Visible = not FullRounded
         }),
-    
+
         Creator.NewRoundFrame(Radius, not FullRounded and "SquircleOutline" or "SquircleOutline2", {
             ThemeTag = {
                 ImageColor3 = Variant ~= "White" and "Outline" or nil,
@@ -87,7 +79,7 @@ function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded,
                 })
             })
         }),
-    
+
         Creator.NewRoundFrame(Radius, "Squircle", {
             Size = UDim2.new(1,0,1,0),
             Name = "Frame",
@@ -95,7 +87,7 @@ function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded,
                 ImageColor3 = Variant ~= "White" and "Text" or nil
             },
             ImageColor3 = Variant == "White" and Color3.new(0,0,0) or nil,
-            ImageTransparency = 1 -- .95
+            ImageTransparency = 1
         }, {
             New("UIPadding", {
                 PaddingLeft = UDim.new(0,16),
@@ -121,7 +113,7 @@ function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded,
             })
         })
     })
-    
+
     Creator.AddSignal(ButtonFrame.MouseEnter, function()
         Tween(ButtonFrame.Frame, .047, {ImageTransparency = .95}):Play()
     end)
@@ -129,16 +121,15 @@ function Button.New(Title, Icon, Callback, Variant, Parent, Dialog, FullRounded,
         Tween(ButtonFrame.Frame, .047, {ImageTransparency = 1}):Play()
     end)
     Creator.AddSignal(ButtonFrame.MouseButton1Up, function()
-        if Dialog then --idk
+        if Dialog then
             Dialog:Close()()
         end
         if Callback then
             Creator.SafeCallback(Callback)
         end
     end)
-    
+
     return ButtonFrame
 end
-
 
 return Button
