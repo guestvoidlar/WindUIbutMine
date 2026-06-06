d066de551ec1a1211f6b26cc4c40616b05a1cf08
undefetaed <undefetaed@gmail.com>
2026-06-06 09:58:06 +0400

    Update Button.lua

--- src/elements/Button.lua
+++ src/elements/Button.lua
@@ -10,6 +10,7 @@ function Element:New(Config)
         Desc = Config.Desc or nil,
         Icon = Config.Icon or "mouse-pointer-click",
         IconThemed = Config.IconThemed or false,
+        IconColor = Config.IconColor or nil,
         Color = Config.Color,
         Justify = Config.Justify or "Between",
         IconAlign = Config.IconAlign or "Right",
@@ -17,17 +18,13 @@ function Element:New(Config)
         Callback = Config.Callback or function() end,
         UIElements = {}
     }
-    
+
     local CanCallback = true
-    
+
     Button.ButtonFrame = require("../components/window/Element")({
         Title = Button.Title,
         Desc = Button.Desc,
         Parent = Config.Parent,
-        -- Image = Config.Image,
-        -- ImageSize = Config.ImageSize,  
-        -- Thumbnail = Config.Thumbnail,
-        -- ThumbnailSize = Config.ThumbnailSize,
         Window = Config.Window,
         Color = Button.Color,
         Justify = Button.Justify,
@@ -39,20 +36,7 @@ function Element:New(Config)
         ElementTable = Button,
         ParentConfig = Config,
     })
-    
-    -- Button.UIElements.ButtonIcon = New("ImageLabel",{
-    --     Image = Creator.Icon("mouse-pointer-click")[1],
-    --     ImageRectOffset = Creator.Icon("mouse-pointer-click")[2].ImageRectPosition,
-    --     ImageRectSize = Creator.Icon("mouse-pointer-click")[2].ImageRectSize,
-    --     BackgroundTransparency = 1,
-    --     Parent = Button.ButtonFrame.UIElements.Main,
-    --     Size = UDim2.new(0,20,0,20),
-    --     AnchorPoint = Vector2.new(1,0.5),
-    --     Position = UDim2.new(1,0,0.5,0),
-    --     ThemeTag = {
-    --         ImageColor3 = "Text"
-    --     }
-    -- })
+
     Button.UIElements.ButtonIcon = Creator.Image(
         Button.Icon,
         Button.Icon,
@@ -62,15 +46,22 @@ function Element:New(Config)
         not Button.Color and true or nil,
         Button.IconThemed
     )
-    
+
     Button.UIElements.ButtonIcon.Size = UDim2.new(0,20,0,20)
     Button.UIElements.ButtonIcon.Parent = Button.Justify == "Between" and Button.ButtonFrame.UIElements.Main or Button.ButtonFrame.UIElements.Container.TitleFrame
     Button.UIElements.ButtonIcon.LayoutOrder = Button.IconAlign == "Left" and -99999 or 99999
     Button.UIElements.ButtonIcon.AnchorPoint = Vector2.new(1,0.5)
     Button.UIElements.ButtonIcon.Position = UDim2.new(1,0,0.5,0)
-    
-    Button.ButtonFrame:Colorize(Button.UIElements.ButtonIcon.ImageLabel, "ImageColor3")
-    
+
+    if Button.IconColor then
+        if Button.ButtonFrame.Uncolorize then
+            Button.ButtonFrame:Uncolorize(Button.UIElements.ButtonIcon.ImageLabel)
+        end
+        Button.UIElements.ButtonIcon.ImageLabel.ImageColor3 = Button.IconColor
+    else
+        Button.ButtonFrame:Colorize(Button.UIElements.ButtonIcon.ImageLabel, "ImageColor3")
+    end
+
     function Button:Lock()
         Button.Locked = true
         CanCallback = false
@@ -81,7 +72,7 @@ function Element:New(Config)
         CanCallback = true
         return Button.ButtonFrame:Unlock()
     end
-    
+
     if Button.Locked then
         Button:Lock()
     end
